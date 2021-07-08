abb028b93b208c00ed095c7d6683dd51b8bc7e17
jonathan343 <jonathangee09@gmail.com>
2021-07-08 02:49:11 -0700

    Added a new loading screen

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
+var wrapper = document.querySelector('.loading-screen svg')
+// We are only adding and removing the 'active' class,
+// the entire animation is defined in the CSS code
+function draw() {
+  wrapper.classList.add('active')
+}
+// Play draw animation once
+setTimeout(draw, 300)
+setTimeout(function() {
+    fade1();
+  }, 2000);
+
+setTimeout(function() {
+    fade2();
+    document.querySelector('.loading-screen').style.display = "none";
+}, 2600);
+
+
+function fade1() {
+var i = 10.0;
+var loading_screen = document.querySelector('.loading-svg')
+loading_screen.style.opacity = 1;
+var k = window.setInterval(function() {
+    if (i <= 0.0) {
+    loading_screen.style.opacity = 0.0;
+    clearInterval(k);
+    } else {
+    loading_screen.style.opacity = i / 10.0;
+    i--;
+    }
+}, 60);
+};
+
+function fade2() {
+    var i = 10.0;
+    var loading_screen = document.querySelector('.loading-screen')
+    loading_screen.style.opacity = 1;
+    var k = window.setInterval(function() {
+        if (i <= 0.0) {
+        loading_screen.style.opacity = 0.0;
+        clearInterval(k);
+        } else {
+        loading_screen.style.opacity = i / 10.0;
+        i--;
+        }
+    }, 40);
+    };
 // Also can pass in optional settings block
 var rellax = new Rellax('.rellax', {
     speed: -2,
