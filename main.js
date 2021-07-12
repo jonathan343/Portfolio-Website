d876d9df7f0438d613dc5f94c9163eb3809f45f7
jonathan343 <jonathangee09@gmail.com>
2021-07-12 03:48:04 -0700

    Added animations and slow scroll

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ var rellax = new Rellax('.rellax', {
     vertical: true,
     horizontal: false
   });
-
+  AOS.init();
 // Closes the navigation after use or touch
 $(document).ready(function () {
     $(document).click(
@@ -69,3 +69,40 @@ $(document).ready(function () {
         }
     );
 });
+
+// Slow Scroll
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1200, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
