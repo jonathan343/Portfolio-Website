f9d5055239a36c3cfb0c8699ad542d8c9d6c7cfe
jonathan343 <jonathangee09@gmail.com>
2022-04-22 04:38:27 -0500

    This is a test for disabling/enabling scrolling

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+
+
+
+
+
+
 var wrapper = document.querySelector('.loading-screen svg')
 // // We are only adding and removing the 'active' class,
 // // the entire animation is defined in the CSS code
@@ -37,6 +85,8 @@ function fade2() {
 $('body').css({
   overflow: 'hidden',
 });
+
+disableScroll();
 setTimeout(draw, 300)
 setTimeout(function() {
     fade1();
@@ -51,6 +101,7 @@ setTimeout(function() {
       overflow: 'auto'
     });
 }, 3400);
+enableScroll();
 
 if ($(window).width() > 960) {
   document.getElementById('home-svg').setAttribute("viewBox", "0 0 1440 1024");
